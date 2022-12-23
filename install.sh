bc17b7446e9d810b7edac8eaced58b99da63fb89
ancodi <121265796+ancodi@users.noreply.github.com>
2022-12-23 15:48:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 read -p "Set Hostname [$(hostname)]: " HOSTNAME
 hostnamectl set-hostname ${HOSTNAME:-$(hostname)}
 
-read -p "Anzeigename für Bluetooth [CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)]: " PRETTY_HOSTNAME
-hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-$(CURRENT_PRETTY_HOSTNAME:-BananaPi)}"
-
 echo "Updating packages"
 apt update
 apt upgrade -y
