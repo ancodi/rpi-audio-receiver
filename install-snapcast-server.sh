8b4dafa8b84abf9067f194e398aad61884c17e22
ancodi <121265796+ancodi@users.noreply.github.com>
2022-12-26 23:08:02 +0100

    Update install-snapcast-server.sh

--- install-snapcast-server.sh
+++ install-snapcast-server.sh
@@ -10,4 +10,4 @@ if [[ "$REPLY" =~ ^(no|n|N)$ ]]; then exit 0; fi
 apt install -y --no-install-recommends snapserver
 
 # Snapserver listening options
-sed -i '/[Stream]/aalsa://?name=<name>&device=hw:0,0[&send_silence=false][&idle_threshold=100][&silence_threshold_percent=0.0]' /etc/snapserver.conf
+sed -i '/[stream]/aalsa://?name=<name>&device=hw:0,0[&send_silence=false][&idle_threshold=100][&silence_threshold_percent=0.0]' /etc/snapserver.conf
