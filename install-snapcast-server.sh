b1e8535642be2727512711627e4362148c86cc95
ancodi <121265796+ancodi@users.noreply.github.com>
2022-12-27 00:01:30 +0100

    Update install-snapcast-server.sh

--- install-snapcast-server.sh
+++ install-snapcast-server.sh
@@ -7,7 +7,10 @@ echo -n "Do you want to install Snapserver? [Y/n] "
 read REPLY
 if [[ "$REPLY" =~ ^(no|n|N)$ ]]; then exit 0; fi
 
-apt install -y --no-install-recommends snapserver
+# Install Snapcast from .deb
+wget https://github.com/badaix/snapcast/releases/download/v0.26.0/snapserver_0.26.0-1_armhf.deb
+
+# apt install -y --no-install-recommends snapserver
 
 # Snapserver listening options
 sed -i '/\[stream\]/a alsa://?name=<name>&device=hw:0,0[&send_silence=false][&idle_threshold=100][&silence_threshold_percent=0.0]' /etc/snapserver.conf
