3c9542068b970828ff109ad44a0441303a8b5517
ancodi <121265796+ancodi@users.noreply.github.com>
2022-12-26 12:40:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 read -p "Set Hostname [$(hostname)]: " HOSTNAME
 hostnamectl set-hostname ${HOSTNAME:-$(hostname)}
 
+CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
+read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
+
 echo "Updating packages"
 apt update
 apt upgrade -y
