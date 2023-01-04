bc41e78165efe726aaceebe44ec1f5550d1574b4
ancodi <121265796+ancodi@users.noreply.github.com>
2023-01-04 23:00:51 +0100

    Update install-snapcast-server.sh

--- install-snapcast-server.sh
+++ install-snapcast-server.sh
@@ -9,8 +9,7 @@ if [[ "$REPLY" =~ ^(no|n|N)$ ]]; then exit 0; fi
 
 # Install Snapcast from .deb
 wget -q https://github.com/badaix/snapcast/releases/download/v0.26.0/snapserver_0.26.0-1_armhf.deb
-apt install ./snapserver_0.26.0-1_armhf.deb
-# apt install -y --no-install-recommends snapserver
+apt install -y --no-install-recommends ./snapserver_0.26.0-1_armhf.deb
 
 # Snapserver listening options
 sed -i '/\[stream\]/a alsa://?name=BTAudioStream&device=hw:0,0[&send_silence=false][&idle_threshold=100][&silence_threshold_percent=0.0]' /etc/snapserver.conf
