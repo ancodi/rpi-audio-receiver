1cca9760a7320f63115ec3834e6441d88b8f7a6f
ancodi <121265796+ancodi@users.noreply.github.com>
2023-01-06 01:26:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,12 @@ apt upgrade -y
 
 echo "Installing components"
 ./install-bluetooth.sh
-./install-shairport.sh
-./install-spotify.sh
+#./install-shairport.sh
 #./install-snapcast-client.sh
 ./install-snapcast-server.sh
+./install-spotify.sh
 #./enable-hifiberry.sh
 
 cat <<EOF > /etc/asound.conf
-defaults.pcm.card 0
-defaults.ctl.card 0
+
 EOF
